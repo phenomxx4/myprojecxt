b57801719cbfa84c1a3cab890b1dc9d6b27d7601
phenomxx4 <phenomxx4@gmail.com>
2025-11-18 16:14:08 +0100

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
+  },
+  compress: true,
+  swcMinify: true,
+}
+
+export default nextConfig
